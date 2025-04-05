90d86932463437bebbc3cb887195227b2256fc3e
Boots-bots <faustobottazzini@gmail.com>
2025-04-05 17:26:48 -0300

    c

---
+++ Vacío.py
@@ -0,0 +1,15 @@
+from settings.imports import*
+from settings.esética import*
+
+folder = "C:/Users/faust/Documents/UBA/Actividades/Laboratorio/4/Datos/"
+
+prueba_fugas = pd.read_csv(folder + "Fugas.csv", index_col=["Tiempo"])
+
+plt.figure()
+plt.title("Prueba de fugas")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Presión [Torr]")
+plt.grid()
+plt.plot(prueba_fugas.index, prueba_fugas["Presión"], ".", label="Medicón")
+plt.legend()
+plt.show(block=True)
--- archivo1.py
+++ archivo1.py
@@ -1,1 +1,9 @@
 #tetas
+# git submodule add https://github.com/Boots-bots/Settings.git settings
+# git submodule status
+# git submodule update --init --recursive
+# git submodule update --remote --merge
+
+from settings.imports import*
+
+a =np.array([1,2,3,4,5])
