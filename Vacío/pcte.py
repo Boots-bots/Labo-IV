a8927f88db296f487b37457aadceb0653f7e1bb2
Boots-bots <faustobottazzini@gmail.com>
2025-04-15 22:21:44 -0300

    shf

--- Vacío/pcte.py
+++ Vacío/pcte.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from settings.imports import*
 from settings.estética import*
+from settings.ajustes import*
 
 folder = "C:/Users/faust/Documents/UBA/Actividades/Laboratorio/4/Datos/"
 
@@ -17,21 +18,98 @@ pcteBD = []
 for i in range(1, 6):
     pcteBD.append(pd.read_csv(folder+ "/Presión/" + f"pcteBD{i}.csv", index_col=["Tiempo"]))
 
-def fuga(t,C,V,p0,pe):
+def fuga(t,CV,p0): # CV = C/V PE = 750
     "C conductancia de predidas"
     "V volumen de la cámara"
     "p0 presión inicial"
     "pe presión externa"
-    return pe + (p0 - pe) * np.exp(-t*C / V)
+    return 750 + (p0 - 750) * np.exp(-t*CV)
 
-def desgase(t,Q,V,p0):
+def desgase(t,QV,p0): # QV = Q/V
     "Q caudal de gas entrante debido a desgase"
     "V volumen de la cámara"
     "p0 presión inicial"
-    return p0 + Q * t / V
+    return p0 + t * QV
 
-def perdidas(t,C,Q,V,p0,pe,F,D):
-    return F*fuga(t,C,V,p0,pe) + D*desgase(t,Q,V,p0) 
+def perdidas(t,CV,QV,p0,c): # CV = C/V ; QV = Q/V ; PE = 750 ; c = fuga  1 - c = desgase
+    return (c)*fuga(t,CV,p0) + (1-c  )*desgase(t,QV,p0) 
+
+def error(df):
+    p = np.array(df["Presión"].values)
+    std = []
+    for y in p:                  # error sensor 
+        # std.append(0.05*y)
+        if y > 7.5e-4:
+            std.append(0.15*y)
+        elif y > 75:
+            std.append(0.05*y)
+        else:
+            std.append(0.30*y)
+
+    for i in range(len(std)-1):  # error temporal
+        a = (p[i+1] - p[i])/np.sqrt(12)
+        # if a > std[i]:
+        std[i] = std[i] + a
+
+    return np.array(std)
+
+# ajustes
+
+Parametros, Errores = [], []
+Chis, Pvals = [], []
+for i in range(len(pcteBM)):
+    pop, cov = curve_fit(perdidas, pcteBM[i].index.values, pcteBM[i]["Presión"].values, sigma = error(pcteBM[i]) , p0=[3e-6,1e-6,pcteBM[i]["Presión"][0],1/2], absolute_sigma=True)
+    Parametros.append(pop)
+    Errores.append(np.sqrt(np.diag(cov)))
+    chi, pv, nu = chi2_pvalor(pcteBM[i].index.values, pcteBM[i]["Presión"].values, error(pcteBM[i]), perdidas(pcteBM[i].index.values, *pop), 2)
+    Chis.append(chi/nu)
+    Pvals.append(pv)
+    # residuos(perdidas, pop, pcteBM[i].index.values, pcteBM[i]["Presión"].values, error(pcteBM[i]), grafico = True)
+
+
+plt.show(block=True)
+
+# resultados
+for i in range(len(pcteBM)): 
+    if i!=1:
+        print("pcte BM", i+1)
+        print("pvalor: ", Pvals[i], "Chi: ", Chis[i])
+        print("C/V: ", Parametros[i][0], "±", Errores[i][0],"Q/V: ", Parametros[i][1], "±", Errores[i][1], "p0: ", Parametros[i][2], "±", Errores[i][2], "c: ", Parametros[i][3], "±", Errores[i][3])
+
+color = ("b","orange","g","k","r")
+
+ejex = np.linspace(0, 1e8, 10000)
+
+plt.figure(figsize=(8, 6))
+# plt.title("pcte BM")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Presión [Torr]")
+plt.grid(color="gray", linestyle="-", linewidth=0.5)
+for i in range(len(pcteBM)):
+    if i != 1:
+        # ejex = np.linspace(np.min(pcteBM[i].index), np.max(pcteBM[i].index), 10000)
+        plt.fill_between(pcteBM[i].index, pcteBM[i]["Presión"] - error(pcteBM[i]), pcteBM[i]["Presión"] + error(pcteBM[i]), alpha=0.3)
+        plt.plot(pcteBM[i].index, pcteBM[i]["Presión"], ".", color = color[i], label=f"Mediciones pcteBM{i+1}")
+        plt.plot(ejex, perdidas(ejex, *Parametros[i]), color = color[i])
+plt.ylim(np.min(pcteBM[i]["Presión"])*0.8, np.max(pcteBM[i]["Presión"])*1.2)
+plt.xlim(np.min(pcteBM[2].index), np.max(pcteBM[2].index))
+plt.legend()
+
+for i in range(len(pcteBM)):
+    if i != 1:
+        ejex = np.linspace(np.min(pcteBM[i].index), np.max(pcteBM[i].index), 10000)
+        plt.figure(figsize=(8, 6))
+        # plt.title(f"pcte BD{i+1}")
+        plt.xlabel("Tiempo [s]")
+        plt.ylabel("Presión [Torr]")
+        plt.grid(color="gray", linestyle="-", linewidth=0.5)
+        plt.fill_between(pcteBM[i].index, pcteBM[i]["Presión"] - error(pcteBM[i]), pcteBM[i]["Presión"] + error(pcteBM[i]), alpha=0.3)
+        plt.plot(pcteBM[i].index, pcteBM[i]["Presión"], ".", color = color[i], label=f"Mediciones pcteBM{i+1}")
+        plt.plot(ejex, perdidas(ejex, *Parametros[i]), color = color[i])
+        # plt.ylim(np.min(pcteBM[i]["Presión"])*0.8, np.max(pcteBM[i]["Presión"])*1.2)
+        plt.legend(loc = "lower right", facecolor='white', framealpha=0.5, fontsize = 12, edgecolor='grey') 
+
+plt.show(block=True)
 
 exit()
 
